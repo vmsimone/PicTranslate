7b66816bdb5e18a859eb8cdde2f4daa4ce3a236d
Vincent Simone <vsimone@protonmail.com>
2018-04-21 14:52:53 -0700

    Improved responsivity and functionality

--- page-render.js
+++ page-render.js
@@ -1,5 +1,6 @@
 function clearSearch() {
-  $('.image-place').html('');
+  $('.image-place-original').html('');
+	$('.image-place-new').html('');
 	 $('.word-place').html('');
 	console.log('html cleared');
 }
@@ -7,13 +8,21 @@ function clearSearch() {
 function readySearch() {
   $('.js-srch').submit(event => {
     event.preventDefault();
-    clearSearch();
-	  console.log('form was submitted');
+	console.log('form was submitted');
     const input = $(event.currentTarget).find('.js-input');
     const query = input.val();
     input.val('');
 	langSelector(query);
+	clearSearch();
   });
+}
+
+function useCountryCode(langCode) {
+	if (langCode === "en") {
+		return "us";
+	} else {
+		return langCode;
+	}
 }
 
 function langSelector(q){
@@ -21,10 +30,10 @@ function langSelector(q){
 	//each needs new arguments to account for the language(s)
 	let userLang = document.querySelector('input[name="user-language"]:checked').value;
 	let foreignLang = document.querySelector('input[name="foreign-language"]:checked').value;
-
-	console.log(userLang);
-	console.log(foreignLang);
+	
 	getTranslationData(q, sortTranslationData, userLang, foreignLang);
+	userLang = useCountryCode(userLang);
+	console.log(userLang);
 	getImageData(q, findImages, userLang);
 }
 
--- word-grabber.js
+++ word-grabber.js
@@ -1,9 +1,25 @@
 const GTRANS_URL = "https://translate.googleapis.com/translate_a/single?";
 
+function foreignSearch(translatedQuery) {
+	let foreignLang = document.querySelector('input[name="foreign-language"]:checked').value;
+	getImageData(translatedQuery, findImages, foreignLang);
+}
+
 function sortTranslationData(data){
 	let userWord = data[0][0][1];
 	let translation = data[0][0][0];
-	$('.word-place').append(`The word "${userWord}" translates to "${translation}"`);
+	$('.word-place').append(`
+	<div class="col-5">
+		<h3>${userWord}</h3>
+	</div> 
+	<div class="col-2">
+		<p>translates to</p> 
+	</div>
+	<div class="col-5">
+		<h3>${translation}</h3>
+	</div>`
+	);
+	foreignSearch(translation);
 }
 
 	function getTranslationData(searchTerm, callback, sourceLang, targetLang) {
