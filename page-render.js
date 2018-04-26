44b59ed6b24fb614d9daac70f10e39cf6ed5f7c1
Vincent Simone <vsimone@protonmail.com>
2018-04-25 19:48:51 -0700

    Changing how page renders

--- page-render.js
+++ page-render.js
@@ -23,7 +23,12 @@ function readySearch() {
 function readyApp() {
   $('#start-btn').on('click', function(){
     loadMain();
-    changePage('main');
+  });
+}
+
+function readyReset() {
+  $('#js-reset').on('click', function() {
+    loadMain();
   });
 }
 
@@ -45,24 +50,6 @@ function langSelector(q){
 	userLang = useCountryCode(userLang);
 	console.log(userLang);
 	getImageData(q, findImages, userLang);
-}
-
-function changePage(nextPage) {
-  console.log('Page changed');
-  switch(nextPage) {
-    case "main":
-
-      break;
-    case "secondLang":
-
-      break;
-    case "transForm":
-      readySearch();
-      break;
-    case "results":
-      loadTranslationPage();
-      break;
-  }
 }
 
 function loadMain() {
@@ -127,45 +114,45 @@ function loadSecondLang() {
   $('main').append(`
     <div class="foreign-lang container col-5">
       <h2>Select a language to translate <em>to</em>:</h2>
-    <form action="#" class="lang-picker">
+      <form action="#" class="lang-picker">
 
-      <label for="English_fl">
-        <input type="radio" name="foreign-language" value="en"  id="English_fl"/>
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png"
-        alt="English" title="English">
-      </label>
+        <label for="English_fl">
+          <input type="radio" name="foreign-language" value="en"  id="English_fl"/>
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png"
+          alt="English" title="English">
+        </label>
 
-      <label for="Spanish_fl">
-        <input type="radio" name="foreign-language" value="es" id="Spanish_fl" />
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/750px-Flag_of_Spain.svg.png"
-        alt="Spanish" title="Spanish">
-      </label>
+        <label for="Spanish_fl">
+          <input type="radio" name="foreign-language" value="es" id="Spanish_fl" />
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/750px-Flag_of_Spain.svg.png"
+          alt="Spanish" title="Spanish">
+        </label>
 
-      <label for="French_fl">
-        <input type="radio" name="foreign-language" value="fr" id="French_fl" />
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/900px-Flag_of_France.svg.png"
-        alt="French" title="French">
-      </label>
+        <label for="French_fl">
+          <input type="radio" name="foreign-language" value="fr" id="French_fl" />
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/900px-Flag_of_France.svg.png"
+          alt="French" title="French">
+        </label>
 
-      <label for="Japanese_fl">
-        <input type="radio" name="foreign-language" value="ja" id="Japanese_fl" />
-        <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/900px-Flag_of_Japan.svg.png"
-        alt="Japanese" title="Japanese">
-      </label>
+        <label for="Japanese_fl">
+          <input type="radio" name="foreign-language" value="ja" id="Japanese_fl" />
+          <img src="https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/900px-Flag_of_Japan.svg.png"
+          alt="Japanese" title="Japanese">
+        </label>
 
-      <label for="Chinese_fl">
-        <input type="radio" name="foreign-language" value="zh-CN" id="Chinese_fl" />
-        <img src="http://flaglane.com/download/chinese-flag/chinese-flag-graphic.png"
-        alt="Chinese" title="Chinese">
-      </label>
+        <label for="Chinese_fl">
+          <input type="radio" name="foreign-language" value="zh-CN" id="Chinese_fl" />
+          <img src="http://flaglane.com/download/chinese-flag/chinese-flag-graphic.png"
+          alt="Chinese" title="Chinese">
+        </label>
 
-      <label for="Korean_fl">
-        <input type="radio" name="foreign-language" value="ko" id="Korean_fl" />
-        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/900px-Flag_of_South_Korea.svg.png"
-        alt="Korean" title="Korean">
-      </label>
+        <label for="Korean_fl">
+          <input type="radio" name="foreign-language" value="ko" id="Korean_fl" />
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/900px-Flag_of_South_Korea.svg.png"
+          alt="Korean" title="Korean">
+        </label>
 
-    </form>
+      </form>
 
     </div>
 
@@ -183,16 +170,15 @@ function loadForm() {
     </div>
     <div class="col-4">
 
-    <h3>Type in your word here:</h3>
-    <form action="#" name="trans-form" class="js-srch">
-    <label for="query"></label>
-    <input type="text" class="js-input" placeholder="e.g. 'Apple'">
-    <button type="submit">Translate!</button>
-    </form>
-    <br>
+      <h3>Type in your word here:</h3>
+      <form action="#" name="trans-form" class="js-srch">
+        <label for="query"></label>
+        <input type="text" class="js-input" placeholder="e.g. 'Apple'">
+        <button type="submit">Translate!</button>
+      </form>
+      <br>
     </div>
-    `);
-    changePage("results");
+  `);
 }
 
 function loadTranslationPage() {
@@ -208,21 +194,22 @@ function loadTranslationPage() {
       <p></p>
     </div>
     <div class="col-4">
+      <h3>Translate another word! (same languages)</h3>
+      <form action="#" name="trans-form" class="js-srch">
+        <label for="query"></label>
+        <input type="text" class="js-input" placeholder="e.g. 'Apple'">
+        <button type="submit">Fast Translate!</button>
 
-    <h3>Translate another word! (same languages)</h3>
-    <form action="#" name="trans-form" class="js-srch">
-    <label for="query"></label>
-    <input type="text" class="js-input" placeholder="e.g. 'Apple'">
-    <button type="submit">Fast Translate!</button>
+        <p>or</p>
 
-    <p>or</p>
-
-    <label for="reset"></label>
-    <button type="submit">Start over</button>
-    </form>
-    <br>
+        <label for="reset"></label>
+        <button type="submit" id="js-reset">Start over</button>
+      </form>
+      <br>
     </div>
     `);
+    readyReset();
+    readySearch();
 }
 
 function readyNewTransation() {
